ece5c21eab06b6c3a150b7e8e8a9c8ae41f96185
Kycb <kycb42148@gmail.com>
2022-01-17 13:48:06 +0300

    Add commands load

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const { Telegraf } = require("telegraf");
 const config = require("./config.json");
+const fs = require("fs");
+const { check } = require("./check");
 
 if (!config.token) {
     console.error("Please, add token to config.json.");
     process.exit(0);
 }
 
+const commands = new Map();
 const bot = new Telegraf(config.token);
+
+const files = fs.readdirSync(`./commands`);
+for (const file of files){
+    const command = require(`./commands/${file}`);
+    if(command.name) commands.set(command.name, command);
+}
+
+for (const [name, command] of commands){
+    bot.hears(command.regex, async(ctx) => {
+        if (await check(ctx)){
+            command.run(ctx);
+        }
+    });
+}
 
 bot.hears("!ping", (ctx) => ctx.reply("Pong!", {reply_to_message_id: ctx.message.message_id}));
 
