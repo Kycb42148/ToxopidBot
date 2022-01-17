1fd1e171d404af03364204048155d08cbe940f60
Kycb <kycb42148@gmail.com>
2022-01-17 13:48:36 +0300

    Add ban command

---
+++ commands/ban.js
@@ -0,0 +1,12 @@
+const { Telegraf } = require("telegraf");
+
+module.exports = {
+    name: "ban",
+    regex: /^(!|\/)(ban|бан)/,
+    perms: "moderation",
+    run: async(ctx) => {
+        const targetId = ctx.message.reply_to_message.from.id;
+        
+        if (await ctx.kickChatMember(targetId)) return ctx.reply("Banned successfully");
+    }
+}
