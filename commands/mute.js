0321fec6ac63f772dc62fa1ec22e0d8bc3a7d075
Кусь Пчелов <kycb42148@gmail.com>
2022-03-06 19:05:06 +0300

    Fix mute

--- commands/mute.js
+++ commands/mute.js
@@ -1,16 +1,18 @@
 module.exports = {
     name: "mute",
-    regex: /^(!|\/)(mute|мьют) ?( ?(\D+)? ?(\d+) ?.+ ?(часов|час|часов|ч|hour|hours|h|минут|минуту|minute|minutes|m|м|дней|день|д|day|days|d|суток|сутки))?/i,
+    regex: /^(!|\/)(mute|мьют) ?((\d+) ?(часов|час|часов|ч|hour|hours|h|минут|минуту|minute|minutes|m|м|дней|день|д|day|days|d|суток|сутки)?)?/i,
     perms: "moderation",
     run: async(ctx) => {
         const targetId = ctx.message.reply_to_message.from.id;
-        const regex = /^(!|\/)(mute|мьют) ?( ?(\D+)? ?(\d+) ?.+ ?(часов|час|часов|ч|hour|hours|h|минут|минуту|minute|minutes|m|м|дней|день|д|day|days|d|суток|сутки))?/i
+        const regex = /^(!|\/)(mute|мьют) ?((\d+) ?(часов|час|часов|ч|hour|hours|h|минут|минуту|minute|minutes|m|м|дней|день|д|day|days|d|суток|сутки)?)?/i
         
         let time = 3600;
-        const count = +ctx.message.text.match(regex)[5] || 1;
+        const count = +ctx.message.text.match(regex)[4] || 1;
+        console.log(ctx.message.text.match(regex)[4]);
+        console.log(ctx.message.text.match(regex)[5]);
 
-        if (['минут', 'минуту', 'minute', 'minutes', 'm', 'м'].includes(ctx.message.text.match(regex)[6])) time = 60;
-        if (['дней', 'день', 'суток', 'сутки', 'day', 'days', 'd', 'д'].includes(ctx.message.text.match(regex)[6])) time = 86400;
+        if (['минут', 'минуту', 'minute', 'minutes', 'm', 'м'].includes(ctx.message.text.match(regex)[5])) time = 60;
+        if (['дней', 'день', 'суток', 'сутки', 'day', 'days', 'd', 'д'].includes(ctx.message.text.match(regex)[5])) time = 86400;
 
         const now = Date.now();
 
