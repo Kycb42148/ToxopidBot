349612f401dafa61115ddf255350961216994543
kycb <kycb42148@gmail.com>
2022-04-09 22:41:44 +0300

    Minor mute/unmute change, A

--- commands/mute.js
+++ commands/mute.js
@@ -1,26 +1,21 @@
 module.exports = {
     name: "mute",
-    regex: /^(!|\/)(mute|мьют) ?((\d+) ?(часов|час|часов|ч|hour|hours|h|минут|минуту|minute|minutes|m|м|дней|день|д|day|days|d|суток|сутки)?)?/i,
+    regex: /^([!\/])(mute|мьют|мут) ?((\d+) ?(час|часов|ч|hour|hours|h|минут|минуту|minute|minutes|m|м|дней|день|д|day|days|d|суток|сутки)?)?/i,
     perms: "moderation",
     run: async(ctx) => {
         const targetId = ctx.message.reply_to_message.from.id;
-        const regex = /^(!|\/)(mute|мьют) ?((\d+) ?(часов|час|часов|ч|hour|hours|h|минут|минуту|minute|minutes|m|м|дней|день|д|day|days|d|суток|сутки)?)?/i
-        
-        let time = 3600;
-        const count = +ctx.message.text.match(regex)[4] || 1;
 
-        if (['минут', 'минуту', 'minute', 'minutes', 'm', 'м'].includes(ctx.message.text.match(regex)[5])) time = 60;
-        if (['дней', 'день', 'суток', 'сутки', 'day', 'days', 'd', 'д'].includes(ctx.message.text.match(regex)[5])) time = 86400;
+        let time = 3600;
+        const count = +ctx.message.text.match(module.exports.regex)[4] || 1;
+
+        if (['минут', 'минуту', 'minute', 'minutes', 'm', 'м'].includes(ctx.message.text.match(module.exports.regex)[5])) time = 60;
+        if (['дней', 'день', 'суток', 'сутки', 'day', 'days', 'd', 'д'].includes(ctx.message.text.match(module.exports.regex)[5])) time = 86400;
 
         const now = Date.now();
 
         if (await ctx.restrictChatMember(targetId, {
             can_send_messages: false,
-            can_send_media_messages: false,
-            can_send_polls: false,
-            can_send_other_messages: false,
-            can_add_web_page_previews: false,
-            until_date: (now / 1000) + count * time
+            until_date: ((now / 1000) + count * time)
         })) return ctx.reply(`Muted until ${new Date(now + count * time * 1000).toUTCString()} successfully`, { reply_to_message_id: ctx.message.message_id });
     }
 }
--- commands/unmute.js
+++ commands/unmute.js
@@ -1,17 +1,12 @@
 module.exports = {
     name: "unmute",
-    regex: /^(!|\/)(unmute|размьют)/i,
+    regex: /^[!\/](unmute|размьют|размут)/i,
     perms: "moderation",
     run: async(ctx) => {
-        console.log("A");
         const targetId = ctx.message.reply_to_message.from.id;
         
         if (await ctx.restrictChatMember(targetId, {
-            can_send_messages: true,
-            can_send_media_messages: true,
-            can_send_polls: true,
-            can_send_other_messages: true,
-            can_add_web_page_previews: true
+            can_send_messages: true
         })) return ctx.reply("Unmuted successfully", { reply_to_message_id: ctx.message.message_id });
     }
 }
