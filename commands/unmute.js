23e44369e1ad8fb11e826257d07ba1e09d7eb6e6
Little cat <79785459+littlecat-dev@users.noreply.github.com>
2022-08-12 20:01:14 +0300

    fix unmute

--- commands/unmute.js
+++ commands/unmute.js
@@ -6,7 +6,12 @@ module.exports = {
         const targetId = ctx.message.reply_to_message.from.id;
         
         if (await ctx.restrictChatMember(targetId, {
-            can_send_messages: true
+            can_send_messages: true,
+            can_send_media_messages: true,
+            can_send_other_messages: true,
+            can_send_polls: true,
+            can_add_web_page_previews: true,
+            can_invite_users: true
         })) return ctx.reply("Unmuted successfully", { reply_to_message_id: ctx.message.message_id });
     }
 }
