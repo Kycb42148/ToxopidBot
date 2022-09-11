2159a693c81b118a68446d18ecb9d6d924a8c442
Kycb42148 <kycb42148@gmail.com>
2022-09-11 17:26:57 +0300

    Implement filter

--- index.js
+++ index.js
@@ -25,6 +25,18 @@ for (const [name, command] of commands){
     });
 }
 
+if (config.filter)
+for (let i = 0; i < config.filter.length; i++) {
+    bot.hears(new RegExp(...config.filter[i]), async(ctx) => {
+        try{
+            if (await ctx.restrictChatMember(ctx.from.id, {
+                can_send_messages: false,
+                until_date: ((Date.now() / 1000) + 300)
+            })) ctx.reply("Your message contains blacklisted word, you will be muted for 5 minutes.", { reply_to_message_id: ctx.message.message_id })
+        } catch {} 
+    });
+}
+
 bot.hears("!ping", (ctx) => ctx.reply("Pong!", {reply_to_message_id: ctx.message.message_id}));
 
 bot.launch(config.token).then(console.log("Launched."));
